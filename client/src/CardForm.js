86ff2ba2387f1a8cb75e6fd94f98485e4e3d052b
vic-bender <vic.bender7@gmail.com>
2026-03-29 12:14:24 -0400

    UI work

--- client/src/CardForm.js
+++ client/src/CardForm.js
@@ -4,10 +4,28 @@ function CardForm() {
     const [front, setFront] = useState('');
     const [back, setBack] = useState('');
 
-    return (
+    function handleSubmit(e) { // talks to backend
+        fetch('http://localhost:5000/api/cards', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ front, back })
+        }).then(() => {setFront(''); setBack('');})
+    }
+
+    return ( // JSX, html "UI"
     <div>
-      <h2>Add a Card</h2>
+        <h2>Add a Card</h2>
+      <input
+          placeholder="Front"
+          value={front}
+          onChange={(e) => setFront(e.target.value)} />
+      <input
+          placeholder="Back"
+          value={back}
+          onChange={(e) => setBack(e.target.value)} />
+      <button onClick={handleSubmit}>Add Card</button>
     </div>
+
   );
 }
 
